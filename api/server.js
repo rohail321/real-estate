64fbe377811abf0bd0f3b710d75ab2c2d9110265
rohail321 <qrohail32@gmail.com>
2023-11-12 19:10:59 +0000

    pushing to server

--- api/server.js
+++ api/server.js
@@ -7,10 +7,12 @@ dotenv.config();
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-
+import path from "path";
 mongoose.connect(process.env.MONGO_URI).then((res) => {
-  console.log("connected");
+  // console.log("connected");
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -23,6 +25,11 @@ app.listen(5000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
